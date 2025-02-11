d143dfbdc16fa91b917cec939493b1ba09db4f3f
Mayanks584 <ranamayank584@gmail.com>
2025-02-11 18:50:22 +0530

    Solution to Logical AND and OR Combination

---
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if (a && b){
+        printf("True\n");
+    else if ( a || b > 0 ){
+        printf("True\n");
+    }
+    else{
+        printf("True\n");
+    }
+    }
+}

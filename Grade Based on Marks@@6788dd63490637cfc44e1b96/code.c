0b275188e8abb6955f61e4b4f3eedd0acd3ba030
Mayanks584 <ranamayank584@gmail.com>
2025-02-13 03:02:20 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -1,21 +1,43 @@
-#include<stdio.h>
-void main(){
-    int a,b,c,d,e,f;
-    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
-    if(a>=90){
+// #include<stdio.h>
+// void main(){
+//     int a,b,c,d,e,f;
+//     scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+//     if(a>=90){
+//         printf("A\n");
+//     }
+//     else if(b>=80 && b<90){
+//         printf("B\n");
+//     }
+//     else if(c>=70 && c<80){
+//         printf("C\n");
+//     }
+//     else if(d>=60 && d<70){
+//         printf("D\n");
+//     }
+//     else{
+//         printf("F\n");
+//     }
+    
+// }
+#include <stdio.h>
+
+void main() {
+    int score;
+    scanf("%d", &score);
+    
+    if (score >= 90) {
         printf("A\n");
-    }
-    else if(b>=80 && b<90){
+    } 
+    else if (score >= 80) {
         printf("B\n");
-    }
-    else if(c>=70 && c<80){
+    } 
+    else if (score >= 70) {
         printf("C\n");
-    }
-    else if(d>=60 && d<70){
+    } 
+    else if (score >= 60) {
         printf("D\n");
-    }
-    else{
+    } 
+    else {
         printf("F\n");
     }
-    
 }

15e095bec1f64b18011eec7592c957c936761e5f
Mayanks584 <ranamayank584@gmail.com>
2025-02-18 13:34:28 +0530

    Solution to Sum of First N Natural Numbers

--- Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
+++ Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
@@ -5,21 +5,21 @@
 //     int sum = a*(a+1)/2;
 //     printf("%d",sum);
 // }
-// #include<stdio.h>
-// void  main(){
-//     int a;
-//     scanf("%d",&a);
-//     int sum = (a*(a+1))/2;
-//     printf("%d",sum);
-// }
-#include<stdio.h>
-void main(){
-    int a, sum = 0;
-    scanf("%d",&a);
-    for( int i=1;i<=a;i++){
-        sum += i;
-  
-    }
+ #include<stdio.h>
+ void  main(){
+     int a;
+     scanf("%d",&a);
+     int sum = (a*(a+1))/2;
      printf("%d",sum);
+ }
+// #include<stdio.h>
+// void main(){
+//     int a, sum = 0;
+//     scanf("%d",&a);
+//     for( int i=1;i<=a;i++){
+//         sum += i;
+  
+//     }
+//      printf("%d",sum);
 
-}
+// }
